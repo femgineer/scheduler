d455f3078c0240db2103ae192cacedd17e568aac
Poornima Vijayashanker <poornima@bizeebee.com>
2014-04-12 14:31:50 -0700

    added show, edit, and update views for schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,10 +1,17 @@
 class SchedulesController < ApplicationController
   def create
+    #todo: check that start_date < end_date and give a message if it's not
     @schedule = Schedule.new(schedule_params)
     @schedule.start_date = Date.strptime(params[:start_date], "%m/%d/%Y")
     @schedule.end_date = Date.strptime(params[:end_date], "%m/%d/%Y")
     @schedule.save
     redirect_to @schedule
+  end
+  
+  def destroy
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+    redirect_to schedules_path
   end
 
   def new
@@ -14,7 +21,12 @@ class SchedulesController < ApplicationController
   end
 
   def edit
+    #todo: can only edit schedules that are in draft mode
     @schedule = Schedule.find(params[:id])
+    @start_date = @schedule.start_date.to_date
+    @end_date = @schedule.end_date.to_date
+    @start_date_str = @start_date.strftime("%m/%d/%Y")
+    @end_date_str = @end_date.strftime("%m/%d/%Y")
   end
   
   def index
@@ -23,10 +35,13 @@ class SchedulesController < ApplicationController
 
   def show
     @schedule = Schedule.find(params[:id])
-  end
+  end    
 
   def update
     @schedule = Schedule.find(params[:id])
+    #todo: find an more elegant solution for this
+    @schedule.start_date = Date.strptime(params[:start_date], "%m/%d/%Y")
+    @schedule.end_date = Date.strptime(params[:end_date], "%m/%d/%Y")
     
     if @schedule.update(schedule_params)
       redirect_to @schedule
